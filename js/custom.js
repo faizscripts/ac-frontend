9dbe7302d15dd458bb2441ac77285ea3cfec50a1
Faiz Ahmed <4faizahmed@gmail.com>
2021-02-09 08:10:59 +0300

    Back to top button

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,15 @@
+// Back to top button
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
+
+
 // Modals
 var myModal = document.getElementById('myModal')
 var myInput = document.getElementById('myInput')
@@ -5,3 +17,5 @@ var myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', function () {
     myInput.focus()
 })
+
+
