0e4239694d3995ccef0f33ff1d1a4033ed6ad4b8
Faiz Ahmed <4faizahmed@gmail.com>
2021-02-12 08:58:15 +0300

    Product view for large screens

--- js/custom.js
+++ js/custom.js
@@ -16,3 +16,28 @@ let year = new Date().getFullYear();
 document.querySelector('#copyright').innerHTML = year;
 
 
+// Product view image
+
+let mainImg = document.querySelector('#prod-main-img');
+let smallImages = document.querySelectorAll('.prod-small-img');
+
+smallImages[0].onclick = function () {
+    mainImg.src = smallImages[0].src;
+}
+smallImages[1].onclick = function () {
+    mainImg.src = smallImages[1].src;
+}
+smallImages[2].onclick = function () {
+    mainImg.src = smallImages[2].src;
+}
+smallImages[3].onclick = function () {
+    mainImg.src = smallImages[3].src;
+}
+smallImages[4].onclick = function () {
+    mainImg.src = smallImages[4].src;
+}
+smallImages[5].onclick = function () {
+    mainImg.src = smallImages[5].src;
+}
+
+
